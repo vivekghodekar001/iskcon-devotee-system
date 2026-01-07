dbdec5420bd0ed7852e4d9124826cd8ff177c9c2
vivekghodekar001 <vivekghodekar001@gmail.com>
2026-01-07 22:57:21 +0530

    Configure PWA Support

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+// Minimal Service Worker to satisfy PWA requirements
+const CACHE_NAME = 'iskcon-portal-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
